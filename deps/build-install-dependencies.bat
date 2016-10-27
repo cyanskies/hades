a4b93c07d07d642c7c36625c60c8e2b0b095f452
Steven Pilkington <thePyro_13@gmx.com>
2016-10-27 16:35:56 +1100

    removed debug bits

--- deps/build-install-dependencies.bat
+++ deps/build-install-dependencies.bat
@@ -3,9 +3,7 @@
 @IF NOT DEFINED generator @set generator="Visual Studio 14 2015"
 @IF NOT DEFINED mode @set mode=Debug
 @IF NOT DEFINED mode2 @set mode2=debug
-IF NOT DEFINED library (@set library=./dep-library) ELSE (set library=../%library%)
-
-pause
+@IF NOT DEFINED library (@set library=./dep-library) ELSE (@set library=../%library%)
 
 @setlocal
 IF [%1]==[] (@set prepath=./ )	ELSE (@set prepath=../%1)

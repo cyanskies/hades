41d80630a3e40ae6c57a6f7ee10b56452d0f1e30
Steven Pilkington <thePyro_13@gmx.com>
2017-02-26 00:20:51 +1100

    started properties interface

---
+++ hades/include/Hades/Properties.hpp
@@ -0,0 +1,15 @@
+#ifndef HADES_PROPERTIES_HPP
+#define HADES_PROPERTIES_HPP
+
+#include "Hades/Types.hpp"
+
+//Properties is a collection of global key->value pairs
+namespace hades
+{
+	namespace console
+	{
+
+	}
+}//hades
+
+#endif //HADES_PROPERTIES_HPP

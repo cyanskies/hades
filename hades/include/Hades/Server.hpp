227da592eeadfc9b26c3a5d12c905faa56bbce0a
Steven Pilkington <thePyro_13@gmx.com>
2017-03-22 19:33:30 +1100

    some notes

--- hades/include/Hades/Server.hpp
+++ hades/include/Hades/Server.hpp
@@ -30,6 +30,8 @@ namespace hades
 		{};
 
 		//the server that hosts actual game logic and update ticking.
+		//this should only be used for command interaction or ticking
+		//normal gameplay calls should go through game_server
 		template<class ClientAction>
 		class local_server : public game_server<ClientAction>
 		{};

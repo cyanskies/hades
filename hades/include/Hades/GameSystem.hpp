57174487507ebb8750cef2dbfac5cb4c40792c25
Steven Pilkington <thePyro_13@gmx.com>
2017-04-01 01:31:14 +1100

    added files for gamesystems

---
+++ hades/include/Hades/GameSystem.hpp
@@ -0,0 +1,14 @@
+#ifndef HADES_GAMESYSTEM_HPP
+#define HADES_GAMESYSTEM_HPP
+
+namespace hades
+{
+	//the interface for game systems.
+	class System
+	{
+	public:
+		
+	};
+}
+
+#endif //HADES_GAMESYSTEM_HPP

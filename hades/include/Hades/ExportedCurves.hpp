bc54c03d0a575b18e1e16ae7274fbbe1fa3614e1
Steven Pilkington <thePyro_13@gmx.com>
2017-04-07 20:42:27 +1000

    added exportedcurves
    this is for exported curve data that can be set to the renderer, or
    accross the network

---
+++ hades/include/Hades/ExportedCurves.hpp
@@ -0,0 +1,36 @@
+#ifndef HADES_EXPORTEDCURVES_HPP
+#define HADES_EXPORTEDCURVES_HPP
+
+#include "SFML/System/Time.hpp"
+
+#include "Hades/Curves.hpp"
+#include "Hades/GameInterface.hpp"
+
+//Exported curves can be streamed into packets,
+//they can also be imported into GameRenderers
+//and into game instances in order to load from file
+namespace hades
+{
+	//exported curves, need their type
+	// curve type
+	// entity and variable id's
+	// 
+	struct ExportedCurves
+	{
+		template<class T>
+		struct ExportSet
+		{
+			EntityId entity;
+			VariableId variable;
+			CurveType type;
+			std::vector<Keyframe<sf::Time, T>> frames;
+		};
+
+		std::vector<std::pair<EntityId, types::string>> entity_names;
+		std::vector<std::pair<VariableId, types::string>> variable_names;
+
+		//each curve type goes here
+	};
+}
+
+#endif //HADES_EXPORTEDCURVES_HPP
---
+++ hades/source/ExportedCurves.cpp
@@ -0,0 +1,2 @@
+#include "Hades/ExportedCurves.hpp"
+

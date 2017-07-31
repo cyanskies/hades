048f26d84b2cbec073caa4a5fc79b2824e05cd83
Steven Pilkington <stevenjpilkington@gmail.com>
2017-07-31 22:31:35 +1000

    more work on build scripts for linux

--- deps/build.sh
+++ deps/build.sh
@@ -1,55 +1,66 @@
 generator=$1
 mode=$2
 library=$3
-prepath=../$4
+prepath="../"$4
 
-if [-z $generator] then
-generator="Unix Makefiles"
+if [ -z ${generator+""}  ] ;
+then generator="Unix Makefiles"
 fi
 
-if [-z $mode] then
-mode="Debug"
+if [ -z ${mode+""} ] ;
+then mode="Debug"
 fi
 
-if [-z $library] then
-library="./dep-library"
+if [ -z ${library+""} ] ;
+then library="./dep-library"
 fi
 
-install-pfx=$prepath$library
+if [ -z ${prepath+""} ] ;
+then prepath="./"
+else prepath="../$prepath"
+fi
+
+installpfx="$prepath$library"
+
+if [ ! -d "$installpfx" ]; then
+mkdir "$installpfx"
+fi
 
 #SFML
 cd ./sfml
-cmake -DCMAKE_BUILD_TYPE=$mode -DCMAKE_INSTALL_PREFIX=../$install-pfx -G $generator
-cmake --build . --target install --config $mode
+cmake -DCMAKE_BUILD_TYPE="$mode" -DCMAKE_INSTALL_PREFIX="../$installpfx" -G "$generator"
+cmake --build . --target install --config "$mode"
 
 #TGUI
 #depends on SFML
 
 cd ../tgui
-cmake -DCMAKE_BUILD_TYPE=$mode -DCMAKE_INSTALL_PREFIX=../$install-pfx -G $generator
-cmake --build . --target install --config $mode
+cmake -DCMAKE_BUILD_TYPE="$mode" -DCMAKE_INSTALL_PREFIX="../$installpfx" -G "$generator"
+cmake --build . --target install --config "$mode"
 
 #yaml-cpp
 
 cd ../yaml-cpp
-cmake -DCMAKE_BUILD_TYPE=$mode -DCMAKE_INSTALL_PREFIX=../$install-pfx -G $generator
-cmake --build . --target install --config $mode
+cmake -DCMAKE_BUILD_TYPE="$mode" -DCMAKE_INSTALL_PREFIX="../$installpfx" -G "$generator"
+cmake --build . --target install --config "$mode"
 
 #minizlib
-cd ../zlib/
+cd ../zlib
 ./configure
 make
-cd ../zlib/contrib
+cd ../zlib/contrib/minizip
 make
 
 #Copy lib and dll files
 
-@IF %mode%==Debug @copy /Y ".\zlib\contrib\vstudio\vc15\x86\ZlibDllDebug\zlibwapi.dll" "%install-pfx%\bin\zlibwapi.dll" 
-@IF %mode%==Debug @copy /Y ".\zlib\contrib\vstudio\vc15\x86\ZlibDllDebug\zlibwapi.lib" "%install-pfx%\lib\zlibwapi.lib"
+#@IF %mode%==Debug @copy /Y ".\zlib\contrib\vstudio\vc15\x86\ZlibDllDebug\zlibwapi.dll" "%install-pfx%\bin\zlibwapi.dll" 
+#@IF %mode%==Debug @copy /Y ".\zlib\contrib\vstudio\vc15\x86\ZlibDllDebug\zlibwapi.lib" "%install-pfx%\lib\zlibwapi.lib"
 
-@IF %mode%==Release @copy /Y ".\zlib\contrib\vstudio\vc15\x86\ZlibDllRelease\zlibwapi.dll" "%install-pfx%\bin\zlibwapi.dll"
-@IF %mode%==Release @copy /Y ".\zlib\contrib\vstudio\vc15\x86\ZlibDllRelease\zlibwapi.lib" "%install-pfx%\lib\zlibwapi.lib"
+#@IF %mode%==Release @copy /Y ".\zlib\contrib\vstudio\vc15\x86\ZlibDllRelease\zlibwapi.dll" "%install-pfx%\bin\zlibwapi.dll"
+#@IF %mode%==Release @copy /Y ".\zlib\contrib\vstudio\vc15\x86\ZlibDllRelease\zlibwapi.lib" "%install-pfx%\lib\zlibwapi.lib"
 
 #copy headers to install dir
-@xcopy /Y ".\zlib\*.h" "%install-pfx%\include\zlib\"
-@xcopy /Y ".\zlib\contrib\minizip\*.h" "%install-pfx%\include\zlib\"
+cp *.h $installpfx/include/zlib
+
+#@xcopy /Y ".\zlib\*.h" "%install-pfx%\include\zlib\"
+#@xcopy /Y ".\zlib\contrib\minizip\*.h" "%install-pfx%\include\zlib\"

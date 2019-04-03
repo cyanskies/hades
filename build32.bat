3d2477ebcbce37904999b5190da14e007317b5b6
Steven Pilkington <cyanskies@users.noreply.github.com>
2019-04-03 20:27:20 +1100

    compile as 64 bit by default

--- build32.bat
+++ build32.bat
@@ -2,4 +2,4 @@
 @IF NOT DEFINED mode @set mode=Debug
 @IF NOT DEFINED library (@set library=./dep-library)
 
-@cmake -DCMAKE_BUILD_TYPE=%mode% -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=%library% -G %generator% -A x64
+@cmake -DCMAKE_BUILD_TYPE=%mode% -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=%library% -G %generator% -A x32
--- build64.bat
+++
@@ -1,5 +0,0 @@
-@IF NOT DEFINED generator @set generator="Visual Studio 15 2017"
-@IF NOT DEFINED mode @set mode=Debug
-@IF NOT DEFINED library (@set library=./dep-library)
-
-@cmake -DCMAKE_BUILD_TYPE=%mode% -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=%library% -G %generator% -A x64

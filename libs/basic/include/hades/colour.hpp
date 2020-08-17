7e5ca22642b9d7e012224276755add1dfb091bf8
Steven Pilkington <cyanskies@users.noreply.github.com>
2020-08-17 13:11:25 +1000

    added more predefined colours

--- libs/basic/include/hades/colour.hpp
+++ libs/basic/include/hades/colour.hpp
@@ -8,30 +8,32 @@
 #include "hades/types.hpp"
 #include "hades/utility.hpp"
 
+//https://www.color-hex.com/ has shade tables for hex colours
+
 namespace hades
 {
 	struct colour
 	{
 		using value_type = uint8;
 
-		uint8& operator[](std::size_t i) noexcept
+		constexpr value_type& operator[](std::size_t i) noexcept
 		{
 			constexpr auto arr = std::array{ &colour::r, &colour::g, &colour::b, &colour::a };
 			assert(i < size(arr));
 			return std::invoke(arr[i], this);
 		}
 
-		const uint8& operator[](std::size_t i) const noexcept
+		constexpr const value_type& operator[](std::size_t i) const noexcept
 		{
 			constexpr auto arr = std::array{ &colour::r, &colour::g, &colour::b, &colour::a };
 			assert(i < size(arr));
 			return std::invoke(arr[i], this);
 		}
 
-		uint8 r{},
+		value_type r{},
 			g{},
 			b{},
-			a{std::numeric_limits<uint8>::max()};
+			a{std::numeric_limits<value_type>::max()};
 	};
 
 	constexpr bool operator==(colour l, colour r) noexcept
@@ -57,14 +59,28 @@ namespace hades
 	namespace colours
 	{
 		constexpr auto black = colour{};
-		constexpr auto white = colour{ std::numeric_limits<uint8>::max(),
-									   std::numeric_limits<uint8>::max(),
-									   std::numeric_limits<uint8>::max() };
+		constexpr auto white = colour{ std::numeric_limits<colour::value_type>::max(),
+									   std::numeric_limits<colour::value_type>::max(),
+									   std::numeric_limits<colour::value_type>::max() };
 
-		constexpr auto transparent = colour{ std::numeric_limits<uint8>::max(),
-											 std::numeric_limits<uint8>::max(),
-											 std::numeric_limits<uint8>::max(),
+		constexpr auto transparent = colour{ std::numeric_limits<colour::value_type>::max(),
+											 std::numeric_limits<colour::value_type>::max(),
+											 std::numeric_limits<colour::value_type>::max(),
 											 0u };
+
+		constexpr auto off_lime = colour{ 186, 218, 85 };
+		constexpr auto mute_cyan = colour{ 127, 229, 240 };
+		constexpr auto red = colour{ 255, 0, 0 };
+		constexpr auto lavender = colour{ 255, 128, 237 };
+		constexpr auto dark_blue = colour{ 64, 114, 148 };
+		constexpr auto dark_cream = colour{ 203, 203, 169 };
+		constexpr auto lawn_green = colour{ 90, 193, 142 };
+		constexpr auto orange = colour{ 255, 165, 0 };
+		constexpr auto gold = colour{ 255, 215, 0 };
+		constexpr auto turquoise = colour{ 64, 224, 208 };
+
+		constexpr auto array = std::array{ off_lime, mute_cyan, red, lavender,
+			dark_blue, dark_cream, lawn_green, orange, gold, turquoise };
 	}
 }
 
